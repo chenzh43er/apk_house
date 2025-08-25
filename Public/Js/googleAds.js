83881dcd6ff02dad42693e79abd6212a5a3c43b2
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-08-26 01:50:33 +0800

    1

---
+++ Public/Js/googleAds.js
@@ -0,0 +1,29 @@
+function returnTeach_adv1_ADS(){
+    return `<!-- teach_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="1945174812"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnTeach_adv2_ADS(){
+    return `<!-- teach_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="2080922090"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnTeach_adv3_ADS(){
+    return `<!-- teach_adv3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="9173762296"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
