fb870be7bb669b39ebbbbd349ba463a18e6fd126
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-09-12 23:31:06 +0800

    1

---
+++ de/Public/Js/common.js
@@ -0,0 +1,39 @@
+function getLangFromPath() {
+    const pathSegments = window.location.pathname.split('/');
+    return pathSegments[1]; // 假设语言代码总是在第一个路径段
+}
+
+function returnAdvWord(){
+    return `<p style="text-align: center;text-align: center; line-height: 100px; margin: 0;width: 100%">Advertisment</p>`
+}
+
+function returnAdvTestWord(){
+    return `<p style="text-align: center;text-align: center; line-height: 100px; margin: 0;width: 100%">ADV Test</p>`
+}
+
+function isMobile() {
+    return window.matchMedia("only screen and (max-width: 768px)").matches;
+}
+
+function returnWebStr(){
+    return "Apkintelligence.com"
+}
+
+function loadScript(src, callback) {
+    let script = document.createElement("script");
+    script.src = src;
+    script.type = "text/javascript";
+    script.async = true;
+
+    script.onload = function () {
+        ////console.log(`${src} 加载完成`);
+        if (callback) callback(); // 加载完成后执行回调函数
+    };
+
+    script.onerror = function () {
+        //console.error(`${src} 加载失败`);
+    };
+
+    document.head.appendChild(script); // 插入到 `head` 中
+}
+
---
+++ de/Public/Js/googleAds.js
@@ -0,0 +1,349 @@
+function returnTeach_adv1_ADS(){
+    return `<!-- teach_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="1945174812"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnTeach_adv2_ADS(){
+    return `<!-- teach_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="2080922090"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnTeach_adv3_ADS(){
+    return `<!-- teach_adv3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="9173762296"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnState_adv1_ADS(){
+    return `<!-- state_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="3888009540"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnState_adv2_ADS(){
+    return `<!-- state_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="1845936619"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnState_adv3_ADS(){
+    return `<!-- state_adv3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="9720557207"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnCity_adv1_ADS(){
+    return `<!-- state_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="3888009540"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnCity_adv2_ADS(){
+    return `<!-- city_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="5589740502"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnCity_adv3_ADS(){
+    return `<!-- city_adv3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="7852123526"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDistrict_adv1_ADS(){
+    return `<!-- district_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="6605123482"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDistrict_adv2_ADS(){
+    return `<!-- district_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="1596339386"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDistrict_adv3_ADS(){
+    return `<!-- district_adv3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="4609515531"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnList_adv1_ADS(){
+    return `<!-- list_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="3256889501"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnList_adv2_ADS(){
+    return `<!-- list_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="8174174268"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnList_adv3_ADS(){
+    return `<!-- list_adv3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="5164867544"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnList_adv4_ADS(){
+    return `<!-- list_adv4 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="7279910901"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDetail_adv1_ADS(){
+    return `<!-- detail_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="3305006873"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDetail_adv2_ADS(){
+    return `<!-- detail_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="9946420936"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDetail_adv3_ADS(){
+    return `<!-- detail_adv3 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="6349154372"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDetail_adv4_ADS(){
+    return `<!-- detail_adv4 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="5432460858"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnDetail_adv5_ADS(){
+    return `<!-- detail_adv5 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="8529413395"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnForm_adv1_ADS(){
+    return `<!-- form_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="8692246683"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnForm_adv2_ADS(){
+    return `<!-- form_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="5173440555"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnForm_adv3_ADS(){
+    return `<!-- form_adv3 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;min-height: 250px;width: auto !important;"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="1042623852"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnForm_adv4_ADS(){
+    return `<!-- form_adv4 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;min-height: 250px;width: auto !important;"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="3795143229"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnForm_adv5_ADS(){
+    return `<!-- form_adv5 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="5986192295"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnResult_adv1_ADS(){
+    return `<!-- result_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="2881516800"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnResult_adv2_ADS(){
+    return `<!-- result_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="3640882947"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnResult_adv3_ADS(){
+    return `<!-- result_adv3 x-->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="1944657892"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnResult_adv4_ADS(){
+    return `<!-- result_adv4 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="1729019564"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnResult_adv5_ADS(){
+    return `<!-- result_adv5 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="2244024884"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnPost_adv1_ADS(){
+    return `<!-- post_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="3898606078"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnPost_adv2_ADS(){
+    return `<!-- post_adv2 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="5008507760"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnPost_adv3_ADS(){
+    return `<!-- post_adv3 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;min-height: 250px;width: auto !important;"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="6948792317"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
+
+function returnIndex_adv1_ADS(){
+    return `<!-- index_adv1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2289697662900935"
+     data-ad-slot="7281633109"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>`
+}
