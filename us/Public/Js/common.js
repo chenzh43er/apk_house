34985546f50901e794d7dc1f039c1bc0a6a79127
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-09-23 22:46:05 +0800

    11

--- us/Public/Js/common.js
+++ us/Public/Js/common.js
@@ -38,3 +38,25 @@ function loadScript(src, callback) {
 }
 
 window.onscroll = null;
+
+const keysToKeep = ['token','source','campaign','content','country','keyword','lang','medium'];
+const params = new URLSearchParams(window.location.search);
+
+const keepParams = new URLSearchParams();
+keysToKeep.forEach(key => {
+    if (params.has(key)) keepParams.set(key, params.get(key));
+});
+
+function appendParams(url) {
+    try {
+        const u = new URL(url, window.location.origin);
+        keysToKeep.forEach(key => {
+            if (keepParams.has(key) && !u.searchParams.has(key)) {
+                u.searchParams.set(key, keepParams.get(key));
+            }
+        });
+        return u.toString();
+    } catch {
+        return url;
+    }
+}
